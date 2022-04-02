76346e494ea88c673fb892ae2308309242c5579b
SebastianDochia <Sebastian.dochia@gmail.com>
2022-04-02 16:21:28 +0300

    feat(webapp): toolbar navigation

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { ItemsPageComponent } from './items-page/items-page/items-page.component';
-import { HomeComponent } from './home/home/home.component';
-import { ItemPageComponent } from './items-page/item-page/item-page/item-page.component';
 import { AdminPageComponent } from './admin/admin-page/admin-page.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { HomeComponent } from './home/home/home.component';
+import {
+  ItemPageComponent,
+} from './items-page/item-page/item-page/item-page.component';
+import {
+  ItemsPageComponent,
+} from './items-page/items-page/items-page.component';
+import { CartComponent } from './cart/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +23,16 @@ import { AdminPageComponent } from './admin/admin-page/admin-page.component';
     ItemsPageComponent,
     HomeComponent,
     ItemPageComponent,
-    AdminPageComponent
+    AdminPageComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatMenuModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ webapp/src/app/app.routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import {
+  RouterModule,
+  Routes,
+} from '@angular/router';
+
+import { AdminPageComponent } from './admin/admin-page/admin-page.component';
+import { CartComponent } from './cart/cart/cart.component';
+import { HomeComponent } from './home/home/home.component';
+import {
+  ItemPageComponent,
+} from './items-page/item-page/item-page/item-page.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { 
+        path: 'catalogue/:id', 
+        component: ItemPageComponent,
+        children : [
+            { path: '../item', component: ItemPageComponent },
+        ]
+    },
+    { path: 'cart', component: CartComponent },
+    { path: 'admin', component: AdminPageComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+]; 
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
